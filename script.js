f1a7fdafc6f7f4385679c0343d511780927829ab
EnzoMSantos <enzosantos2502@gmail.com>
2023-02-13 13:57:21 -0300

    projeto finalizado

--- script.js
+++ script.js
@@ -14,3 +14,18 @@ sizePassword.innerHTML = sliderElement.value
 slider.oninput = function() {
     sizePassword.innerHTML = this.value
 }
+
+function GeneratePassword() {
+    let pass = ""
+
+    for(let i = 0, n = charset.length; i < sliderElement.value; i++) {
+        pass += charset.charAt(Math.floor(Math.random() * n))
+    }
+    containerPassword.classList.remove("hide")
+    password.innerHTML = pass
+}
+
+function copyPassword() {
+    alert("Senha copiada com sucesso!")
+    navigator.clipboard.writeText(novaSenha)
+}
